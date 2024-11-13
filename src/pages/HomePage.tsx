91bd7e6305526f02b91bd80a63073dc5ef5b0c73
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-13 12:01:37 -0600

    creating home component and separate it in others components, to separate the logic

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,18 @@
+import HomeBodyComponent from "@/components/petProject/HomeBody";
+import HomeNavbarComponent from "@/components/petProject/HomeNavbar";
 import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="div">Home Page</div>
+    <div className="w-full h-screen overflow-hidden relative flex flex-col">
+      <img className="static object-cover object-top w-full h-full opacity-75" src="src\assets\homeEffectP.svg" />
+      <div className="w-full absolute">
+        <HomeNavbarComponent />
+      </div>
+      <div className="w-full h-full absolute top-12 flex flex-col items-center justify-center">
+        <HomeBodyComponent/>
+      </div>
+    </div>
   );
 }
 
